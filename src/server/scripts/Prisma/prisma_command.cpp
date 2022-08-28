a2389724a9c704ecfb599eca0f93e724807ec87f
EIntemporel <dev.intemporel@gmail.com>
2022-08-28 18:52:00 +0200

    Update prisma_command.cpp
    Health & Move Info

--- src/server/scripts/Prisma/prisma_command.cpp
+++ src/server/scripts/Prisma/prisma_command.cpp
@@ -18,10 +18,17 @@ public:
 
     ChatCommandTable GetCommands() const override
     {
+        static ChatCommandTable prismaMoveCommandTable =
+        {
+            { "info",       HandleMoveInformation,      rbac::RBAC_PERM_COMMAND_GM,     Console::No },
+        };
+
         static ChatCommandTable gmCommandTable =
         {
             { "list",       HandlePrismaList,           rbac::RBAC_PERM_COMMAND_GM,        Console::No },
             { "invoke",     HandlePrismaInvoke,         rbac::RBAC_PERM_COMMAND_GM,        Console::No },
+            { "health",     HandlePrismaHealth,         rbac::RBAC_PERM_COMMAND_GM,        Console::No },
+            { "move",       prismaMoveCommandTable },
         };
         static ChatCommandTable commandTable =
         {
@@ -123,6 +130,64 @@ public:
         }
 
         return false;
+    }
+
+    static bool HandlePrismaHealth(ChatHandler* handler, Optional<bool> enableArg)
+    {
+        Player* player = handler->GetSession()->GetPlayer();
+
+        PrismaDatabasePreparedStatement* stmt = PrismaDatabase.GetPreparedStatement(PRISMA_SEL_PLAYER_PRISMA);
+        stmt->setUInt32(0, player->GetGUID());
+
+        PreparedQueryResult result = PrismaDatabase.Query(stmt);
+
+        if (result)
+        {
+            for (int i = 0; i < 6; ++i)
+            {
+                int guid = (*result)[i].GetUInt32();
+                if (guid <= 0)
+                    continue;
+
+                Prisma* _prisma = new Prisma();
+                if (!_prisma->InitializePrismaFromGuid(guid))
+                    continue;
+
+                _prisma->RestoreStamina();
+                _prisma->RestoreMove();
+                _prisma->RestoreStatus();
+                _prisma->SavePrismaToDB();
+
+                //_prisma->CleanupsBeforeDelete();
+                delete _prisma;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    static bool HandleMoveInformation(ChatHandler* handler, uint32 move_id)
+    {
+        const PrismaMoveTemplate* _move = sObjectMgr->GetPrismaMoveTemplate(move_id);
+        if (!_move)
+        {
+            std::string data = "Move with id " + std::to_string(move_id) + " doesn't exist.";
+            handler->SendSysMessage(std::string_view{ data });
+            return true;
+        }
+
+        handler->SendSysMessage(std::string("=== " + _move->Name + " (" + std::to_string(_move->Entry) + ") ==="));
+        handler->SendSysMessage(std::string("Spell trigger : " + std::to_string(_move->SpellTrigger)));
+        handler->SendSysMessage(std::string("Type : " + std::to_string(uint32(_move->Type)) + ", Category : " + std::to_string(uint32(_move->Category))));
+        handler->SendSysMessage(std::string("PP : " + std::to_string(_move->PowerPoints) + ", Base Power : " + std::to_string(_move->BasePower)));
+        handler->SendSysMessage(std::string("Accuracy : " + std::to_string(_move->Accuracy) + ", CritRate : " + std::to_string(_move->CritRate)));
+        handler->SendSysMessage(std::string("Speed priority : " + std::to_string(_move->SpeedPriority) + ", Selection type : " + std::to_string(uint32(_move->SelectionType))));
+        handler->SendSysMessage(std::string("NonVolatile flag : " + std::to_string(_move->NonVolatileStatusFlags) + ", Proba : " + std::to_string(_move->ProbabilityNVSF)));
+        handler->SendSysMessage(std::string("Volatile flag : " + std::to_string(_move->VolatileStatusFlags) + ", Proba : " + std::to_string(_move->ProbabilityVSF)));
+        handler->SendSysMessage(std::string("VolatileCombat flag : " + std::to_string(_move->VolatileCombatStatusFlags) + ", Proba : " + std::to_string(_move->ProbabilityVCSF)));
+        return true;
     }
 };
 
